31eb2e20062b418a0fce7edc482df200cf604b7b
Akmal <akmal199818@gmail.com>
2023-05-18 18:38:33 +0500

    commit

--- src/main/java/com/example/demo/apps/JavaContextApp.java
+++ src/main/java/com/example/demo/apps/JavaContextApp.java
@@ -1,34 +1,25 @@
 package com.example.demo.apps;
 
+import com.example.demo.Interface.Action;
 import com.example.demo.beans.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class JavaContextApp {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfiguration.class);
+        HashMap<Integer, Action> map = new HashMap<>();
+        map.put(1, context.getBean("music",MusicAction.class));
+        map.put(2, context.getBean("film",FilmAction.class));
+        map.put(3, context.getBean("game",GameAction.class));
         Scanner scanner = new Scanner(System.in);
         System.out.println("Выберите действие:\n1 - Включить музыку\n2 - Включить фильм\n3 - Включить игру");
         int actionType = scanner.nextInt();
-
-        if (actionType == 1) {
-            MusicAction musicAction = context.getBean("music", MusicAction.class);
-            musicAction.play();
-        }
-        if (actionType == 2) {
-            FilmAction filmAction = context.getBean("film", FilmAction.class);
-            filmAction.play();
-        }
-        if (actionType == 3) {
-            GameAction gameAction = context.getBean("game", GameAction.class);
-            gameAction.play();
-        }
-        else{
-            System.out.println("Ошибка");
-        }
-
+        map.get(actionType).play();
+        
         }
     }
 
